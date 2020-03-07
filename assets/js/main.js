9a4e72b51a8b25e682cd163d246e440628e63dec
Roy <royxuhan@gmail.com>
2020-03-06 22:04:49 -0400

    Calculate price based on item quantity

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,12 @@ function selectOption(id) {
     var element = document.getElementById(id);
     element.classList.toggle("options-selected");
 }
+
+// Calculate how much the total is based on item pricing and quantity
+function calculatePrice() {
+    var itemPrice = 3;
+    var quantityForm = document.getElementById("quantity");
+    var numItems = quantityForm.options[quantityForm.selectedIndex].value;
+    var total = itemPrice * numItems;
+    document.getElementById("pricing-total").innerHTML = "Total: $" + total.toFixed(2);
+}
