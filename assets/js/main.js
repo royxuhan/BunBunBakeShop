ede85b26b0f1ec6c6ef6fccf468542d3401e8544
Roy <royxuhan@gmail.com>
2020-03-06 22:24:28 -0400

    Can only select one flavor or one glazing at a time

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,32 @@
 <!-- Spring 2020 --> 
 */
 
-// Highlight selected option on product detail page
-function selectOption(id) {
+// Highlight selected flavor option on product detail page
+function selectFlavorOption(id) {
     var element = document.getElementById(id);
-    element.classList.toggle("options-selected");
+    element.classList.toggle("flavor-selected");
+
+    var selectedOptions = document.querySelectorAll('.flavor-selected');
+    for (var i = 0; i < selectedOptions.length; i++) {
+        var previousSelected = document.getElementById(selectedOptions[i].id);
+        if (previousSelected != element) {
+            previousSelected.classList.toggle("flavor-selected");
+        }
+    }
+}
+
+// Highlight selected glazing option on product detail page
+function selectGlazingOption(id) {
+    var element = document.getElementById(id);
+    element.classList.toggle("glazing-selected");
+
+    var selectedOptions = document.querySelectorAll('.glazing-selected');
+    for (var i = 0; i < selectedOptions.length; i++) {
+        var previousSelected = document.getElementById(selectedOptions[i].id);
+        if (previousSelected != element) {
+            previousSelected.classList.toggle("glazing-selected");
+        }
+    }
 }
 
 // Calculate how much the total is based on item pricing and quantity
