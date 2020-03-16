a63c25309701edeaa64e628becc4562b26bf8f20
Roy <royxuhan@gmail.com>
2020-03-15 22:56:45 -0400

    Change product details based on selections

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,52 +6,62 @@
 
 // Highlight selected flavor option on product detail page
 function selectFlavorOption(id) {
-    var element = document.getElementById(id);
-    element.classList.toggle("flavor-selected");
+  var element = document.getElementById(id);
+  element.classList.toggle("flavor-selected");
 
-    var selectedOptions = document.querySelectorAll('.flavor-selected');
-    for (var i = 0; i < selectedOptions.length; i++) {
-        var previousSelected = document.getElementById(selectedOptions[i].id);
-        if (previousSelected != element) {
-            previousSelected.classList.toggle("flavor-selected");
-        }
+  var selectedOptions = document.querySelectorAll(".flavor-selected");
+  for (var i = 0; i < selectedOptions.length; i++) {
+    var previousSelected = document.getElementById(selectedOptions[i].id);
+    if (previousSelected != element) {
+      previousSelected.classList.toggle("flavor-selected");
     }
+  }
 }
 
 // Highlight selected glazing option on product detail page
 function selectGlazingOption(id) {
-    var element = document.getElementById(id);
-    element.classList.toggle("glazing-selected");
+  var element = document.getElementById(id);
+  element.classList.toggle("glazing-selected");
 
-    var selectedOptions = document.querySelectorAll('.glazing-selected');
-    for (var i = 0; i < selectedOptions.length; i++) {
-        var previousSelected = document.getElementById(selectedOptions[i].id);
-        if (previousSelected != element) {
-            previousSelected.classList.toggle("glazing-selected");
-        }
+  var selectedOptions = document.querySelectorAll(".glazing-selected");
+  for (var i = 0; i < selectedOptions.length; i++) {
+    var previousSelected = document.getElementById(selectedOptions[i].id);
+    if (previousSelected != element) {
+      previousSelected.classList.toggle("glazing-selected");
     }
+  }
 }
 
 // Update product heading based on selections
 function updateProductTitle() {
-    var flavor = document.querySelectorAll('.flavor-selected')[0];
+  var flavor = document.querySelectorAll(".flavor-selected")[0];
+  var glazing = document.querySelectorAll(".glazing-selected")[0];
+
+  if (flavor != null && glazing == null) {
     var flavorText = flavor.innerHTML;
-    document.getElementById("product-title").innerHTML = "Cinnamon Rolls" + "<br />" + "Flavor: " + flavorText;
-    var glazing = document.querySelectorAll('.glazing-selected')[0];
-    if (glazing == null) {
-        var glazingText = "None";
-    } else {
-        var glazingText = glazing.innerHTML;
-    }
-    var oldHTML = document.getElementById("product-title").innerHTML;
-    document.getElementById("product-title").innerHTML = oldHTML + "<br />" + "Glazing: " + glazingText;
+    document.getElementById("product-title").innerHTML =
+      "Cinnamon Rolls" + "<br />" + "Flavor: " + flavorText;
+  } else if (glazing != null && flavor == null) {
+    var glazingText = glazing.innerHTML;
+    document.getElementById("product-title").innerHTML =
+      "Cinnamon Rolls" + "<br />" + "Glazing: " + glazingText;
+  } else if (flavor != null && glazing != null) {
+    var flavorText = flavor.innerHTML;
+    var glazingText = glazing.innerHTML;
+    document.getElementById(
+      "product-title"
+    ).innerHTML = `Cinnamon Rolls<br />Flavor: ${flavorText}<br />Glazing: ${glazingText}`;
+  } else {
+    document.getElementById("product-title").innerHTML = "Cinnamon Rolls";
+  }
 }
 
 // Calculate how much the total is based on item pricing and quantity
 function calculatePrice() {
-    var itemPrice = 3;
-    var quantityForm = document.getElementById("quantity");
-    var numItems = quantityForm.options[quantityForm.selectedIndex].value;
-    var total = itemPrice * numItems;
-    document.getElementById("pricing-total").innerHTML = "Total: $" + total.toFixed(2);
+  var itemPrice = 3;
+  var quantityForm = document.getElementById("quantity");
+  var numItems = quantityForm.options[quantityForm.selectedIndex].value;
+  var total = itemPrice * numItems;
+  document.getElementById("pricing-total").innerHTML =
+    "Total: $" + total.toFixed(2);
 }
